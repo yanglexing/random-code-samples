c99e2db519bed86597e024a17d11f775acb9b6e2
Lexing Yang <lexing.yang@SF-M-LYang2.glu.com>
2015-07-02 11:15:44 -0700

    functionalized

--- src/main/python/euler1.py
+++ src/main/python/euler1.py
@@ -1,5 +1,9 @@
-sum = 0
-for i in range(1000):
-	if (i % 3 == 0 or i % 5 == 0):
-		sum += i
-print sum	
+def sum_multiples():
+	sum = 0
+	for i in range(1000):
+		if (i % 3 == 0 or i % 5 == 0):
+			sum += i
+	return sum
+
+if __name__=="__main__":
+	print sum_multiples()	
